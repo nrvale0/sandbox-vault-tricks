f8efdbd5ebfd3515f88b1cddebc6cba174520a3a
Nathan R Valentine <nrvale00@gmail.com>
2017-07-07 15:03:28 -0700

    fix for postgres conn URL

--- scripts/vault-config-database.sh
+++ scripts/vault-config-database.sh
@@ -11,7 +11,7 @@ printf "\nMounting Vault database backend..."
 
 printf "\nWriting the Postgres database backend config..."
 (set -x; 
-	vault write database/config/postgresql plugin_name=postgresql-database-plugin allowed_roles="readonly" connection_url="postgresql://postgres:postgres@postgres:5432/?sslmode=disable")
+	vault write database/config/postgresql plugin_name=postgresql-database-plugin allowed_roles="readonly" connection_url="postgresql://postgres:postgres@postgres:5432/testdb?sslmode=disable")
 
 printf "\nCreating a read-only policy to associate with credentials..."
 (set -x; 
@@ -19,7 +19,7 @@ printf "\nCreating a read-only policy to associate with credentials..."
 		db_name=postgresql \
 		creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; \
 		GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
-		default_ttl="1m" \
+		default_ttl="30m" \
     		max_ttl="1h")
 
 printf "\nRead the read-only account credentials..."
